576fddec2bf35f71fce3b8e6327e0da0fcc5107b
minhtt1343 <minhtt1343@ut.edu.vn>
2026-09-17 15:30:03 +0700

    Cap nhat ket noi PgBouncer va sua DocumentController

--- backend/backend/src/main/java/com/livingdocs/backend/controller/DocumentController.java
+++ backend/backend/src/main/java/com/livingdocs/backend/controller/DocumentController.java
@@ -1,18 +1,25 @@
 package com.livingdocs.backend.controller;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.livingdocs.backend.model.AuditTrail;
 import com.livingdocs.backend.model.Document;
 import com.livingdocs.backend.repository.AuditTrailRepository;
 import com.livingdocs.backend.repository.DocumentRepository;
 import com.livingdocs.backend.service.GithubPushService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/documents")
